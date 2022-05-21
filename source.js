9ddedc31db95d1c5c386dfb6b1591966c8b4b88c
Joshua Bray <josh.c.bray@gmail.com>
2022-05-21 15:07:21 +0700

    Add rainbow and shading buttons
    Content has basic functionality and the code has been reformatted for
    ease of reading for the programmer. Variables have been more
    appropriately named.

--- source.js
+++ source.js
@@ -1,44 +1,42 @@
 // Identify where divs will be inserted
 const container = document.querySelector(".container");
-console.log(container)
 
-// Create 16 divs and insert
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  div.classList.add("box", `${i}`);
-  div.id = `${i}`;
-  div.textContent = `test${i}`;
-  container.appendChild(div);
-};
+// Global variables
+const ogContainerWidth = document.querySelector(".container").offsetWidth;
+let mode = "default";
+let opacity;
 
-// Hover
-const boxes = document.querySelectorAll("div.box");
-boxes.forEach((box) => {
-  box.addEventListener("mouseenter", highlight);
-});
+// Initialise grid
+const initWidth = 4;
+createNewGrid(initWidth);
 
-function highlight(e) {
-  let key = e.target.getAttribute("id");
-  // This doens't work for some reason
-  // const boxNode = document.querySelector(`div.${key}`);
-  const boxNode = document.getElementById(`${key}`);
-  boxNode.style.backgroundColor = "green";
+const btnRainbow = document.querySelector("button.rainbows");
+btnRainbow.addEventListener("click", rainbow);
+
+const btnShade = document.querySelector("button.shade");
+btnShade.addEventListener("click", shade);
+
+const btn = document.querySelector("button.change");
+btn.addEventListener("click", generateNewGrid);
+
+function rainbow() {
+  mode = "rainbow";
 }
 
-// button to prompt user for display size
-// limit of 100 (do while loop)
-const btn = document.querySelector("button");
-btn.addEventListener("click", promptUser);
-
-function promptUser() {
-  let width;
-  do {
-    width = prompt("Pick a number between 1-100 for grid width");
-  } while (width > 100 || width < 1);
-  removeChildren();
+function shade() {
+  mode = "shade";
+  opacity = 0.1;
 };
 
-// remove existing grid
+function generateNewGrid() {
+  let containerWidth;
+  do {
+    containerWidth = prompt("Pick a number between 1-100 for grid width");
+  } while (containerWidth > 100 || containerWidth < 1);
+  removeChildren();
+  createNewGrid(containerWidth);
+};
+
 function removeChildren() {
   let child = container.lastElementChild; 
   while (child) {
@@ -46,11 +44,73 @@ function removeChildren() {
     child = container.lastElementChild;
   };
 };
-// retain original dimensions of grid
 
-// width of new boxes = container.width / width rounded down.
-// calculate the difference between OG cont.width and new cont.width
-// add padding aroung container for consistency.
+function createNewGrid(numCellsWide) {
+  let totalGridElems = numCellsWide * numCellsWide;
+  const cellWidth = defineBoxWidth(numCellsWide);
 
-// replace with new grid of x dimension
+  // Create multiple divs
+  for (let i = 0; i < totalGridElems; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+    div.id = `${i}`;
 
+    // Set box dimensions
+    div.style.width = `${cellWidth}px`;
+    div.style.height = `${cellWidth}px`;
+    container.appendChild(div);
+  };
+  // Add color to boxes
+  addHover();
+  // center the container contents
+  setContainerBuffer(numCellsWide);
+};
+
+function addHover() {
+  const boxes = document.querySelectorAll("div.box");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseenter", highlight);
+  });
+};
+
+function highlight(e) {
+  let key = e.target.getAttribute("id");
+  const boxNode = document.getElementById(`${key}`);
+
+  // consider new button modes
+  if (mode == "rainbow") {
+    boxNode.style.backgroundColor = getRandomColor();
+  } else if (mode == "shade") {
+    // Identify the opacity of node
+    let alpha = Number(boxNode.style.backgroundColor.replace(/^.*,(.+)\)/,'$1'));
+    if (alpha < 1) {
+      alpha += 0.1;
+    }
+    boxNode.style.backgroundColor = `rgba(0, 0, 0, ${alpha})`;
+  } else {
+    boxNode.style.backgroundColor = "black";
+  }
+}
+
+function setContainerBuffer(numCellsWide) {
+  let boxWidth = defineBoxWidth(numCellsWide);
+  let contWidthDifference = ogContainerWidth - (boxWidth * numCellsWide);
+  container.style.padding = `${contWidthDifference/2}px`;
+};
+
+function defineBoxWidth(numCellsWide) {
+  let boxWidth = Math.floor(ogContainerWidth/numCellsWide);
+  return boxWidth;
+};
+
+// Create random color helper1
+const getRandomNumber = (limit) => {
+  return Math.floor(Math.random() * limit);
+};
+// Create random color helper2
+const getRandomColor = () => {
+  const h = getRandomNumber(360);
+  const s = getRandomNumber(100);
+  const l = getRandomNumber(100);
+  return `hsl(${h}deg, ${s}%, ${l}%)`;
+};
